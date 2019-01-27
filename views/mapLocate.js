a76359de53e71601b45739d99b9909ad047ed937
dabris <dabris16.x@gmail.com>
2019-01-26 20:52:21 -0500

    mapLocate

---
+++ views/mapLocate.js
@@ -0,0 +1,26 @@
+var pos;
+if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(function(position) {
+              pos = {
+              lat: position.coords.latitude,
+              lng: position.coords.longitude
+            };
+
+            infoWindow.setPosition(pos);
+            infoWindow.setContent('Location found.');
+            infoWindow.open(map);
+            map.setCenter(pos);
+          }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+          });
+        } else {
+          handleLocationError(false, infoWindow, map.getCenter());
+        }
+      }
+function calcLat(){
+    
+}
+
+function calcLon(){
+    
+}
