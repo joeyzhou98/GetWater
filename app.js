841105e3db302524bdaf1608cc9031f68cd884eb
Joey Zhou <joeyzhou7002@gmail.com>
2019-01-27 00:36:39 +0000

    add lat long retrieval from mongolab

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 require("dotenv").config();
 
 var express = require("express");
-var Fountain = require("models/fountain");
+var Fountain = require("./models/fountain");
 var mongoose = require("mongoose");
 var app = express();
 
 mongoose.connect(process.env.DATABASEURL);
 
 app.get("/", function(req, res) {
-    res.render("mainPage.ejs");
+    Fountain.findOne({}), function(err, allFountains) {
+        if (err) {
+            console.log(err) 
+        } else {
+            res.render("mainPage.ejs", {fountains: allFountains});
+        }
+    }
 });
 
 app.listen(process.env.PORT, process.env.IP, function() {
