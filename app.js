85ce910a00d2f5201ba93bc061512758801f4ed8
Joey Zhou <joeyzhou7002@gmail.com>
2019-01-27 08:45:07 +0000

    added new post route

--- app.js
+++ app.js
@@ -5,6 +5,16 @@ var Fountain = require("./models/fountain");
 var mongoose = require("mongoose");
 var app = express();
 var path = require("path");
+var NodeGeocoder = require('node-geocoder');
+ 
+var options = {
+  provider: 'google',
+  httpAdapter: 'https',
+  apiKey: process.env.GEOCODER_API_KEY,
+  formatter: null
+};
+
+var geocoder = NodeGeocoder(options);
 
 mongoose.connect(process.env.DATABASEURL);
 
@@ -19,6 +29,26 @@ app.get("/", function(req, res) {
         } else {
             res.render("GetWaterFrontpage.html", {fountains: allFountains});
         }
+    });
+});
+
+app.post("/", function(req, res) {
+    var description = req.body.description;
+    geocoder.geocode(req.body.location, function (err, data) {
+        if (err || !data.length) {
+          return res.redirect('back');
+        }
+        var lat = data[0].latitude;
+        var lng = data[0].longitude;
+        var location = data[0].formattedAddress;
+        var newFountain = {description: description, location: location, lat: lat, lng: lng};
+        Fountain.create(newFountain, function(err, fountain) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.redirect("/");
+            }
+        });
     });
 });
 
